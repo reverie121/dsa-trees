6a9451db4e3914b338b6b94e3f66bedc9bcd5df1
Jeremy Sobczak <code@jerspace.com>
2023-07-29 16:56:57 -0400

    Added methods for Tree class.

--- tree.js
+++ tree.js
@@ -5,6 +5,7 @@ class TreeNode {
     this.val = val;
     this.children = children;
   }
+  
 }
 
 class Tree {
@@ -15,20 +16,72 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+    if (this.root === null) return 0;
+
+    let sum = 0;
+
+    let toVisitQueue = [this.root];
     
+    while (toVisitQueue.length) {
+        let current = toVisitQueue.shift();
+
+        sum += current.val;
+
+        for (let child of current.children) {
+            toVisitQueue.push(child)
+        }
+    }      
+
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (this.root === null) return 0;
 
-  }
+    let count = 0;
+
+    let toVisitQueue = [this.root];
+    
+    while (toVisitQueue.length) {
+        let current = toVisitQueue.shift();
+
+        if (current.val % 2 == 0) {
+            count += 1;
+        }
+
+        for (let child of current.children) {
+            toVisitQueue.push(child)
+        }
+    }       
+
+    return count;
+}
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (this.root === null) return 0;
 
+    let count = 0;
+
+    let toVisitQueue = [this.root];
+    
+    while (toVisitQueue.length) {
+        let current = toVisitQueue.shift();
+
+        if (current.val > lowerBound) {
+            count += 1;
+        }
+
+        for (let child of current.children) {
+            toVisitQueue.push(child)
+        }
+    }       
+
+    return count;
   }
 }
 
